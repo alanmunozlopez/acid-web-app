e923d1249827dab1ab39a750a41026d8abfa8f5d
Alan Munoz Lopez <alanmunozlopez@Alans-MacBook-Pro.local>
2019-03-05 12:31:36 -0300

    AM - maps logic added, latlng and marker

--- src/pages/container/index.js
+++ src/pages/container/index.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
 
-class App extends Component {
+export class MapContainer extends Component {
+
+  state = {
+    location: {}
+  }
+
+  onMarkerClick = async (mapProps, map, event) => {
+    let location = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    };
+    this.setState({location});
+    console.log(location);
+  }
+
   render() {
     return (
-      <div className="App">
-        <p>HEY!</p>
-        <p>HEY!</p>
-        <p>HEY!</p>
-        <p>HEY!</p>
-        <p>HEY!</p>
-        <p>HEY!</p>
-      </div>
+      <Map
+        google={this.props.google}
+        zoom={3}
+        style={mapStyles}
+        onClick={this.onMarkerClick}
+      >
+        {
+          this.state.location ?
+          <Marker
+            position={this.state.location}
+          /> :
+          null
+        }
+      </Map>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyC4d_SQXrjUDZAtq9F7pUuzef9SkAveBaY'
+})(MapContainer);
